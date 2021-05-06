f042305420402b7ea1968f1272bf36026c3c6d85
Emdadul Islam <58410798+mdadul@users.noreply.github.com>
2021-05-06 15:14:16 +0600

    Data Type

---
+++ DataType.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+  var x int = 42
+  var y float32 = 1.376666
+  var name string = "Emdadul"
+  var online bool = true
+
+  fmt.Println(name)
+  fmt.Println(x)
+  fmt.Println(y)
+  fmt.Println(online)
+  
+}
