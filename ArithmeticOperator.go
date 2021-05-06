acbd7e1412dc8179b80c1e85a9d4bdff4586b576
Emdadul Islam <58410798+mdadul@users.noreply.github.com>
2021-05-06 15:16:11 +0600

    Arithmetic operator

---
+++ ArithmeticOperator.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  x := 45
+  y := 7
+  
+  //Addition
+  add := x+y
+  fmt.Println(add)
+  
+  //Subtraction
+  sub := x-y
+  fmt.Println(sub)
+  
+  //Multipication
+  mul := x*y
+  fmt.Println(mul)
+  
+  //Division 
+  div := x/y
+  fmt.Println(div)
+      
+  // Modulus, results in the remainder of the division
+  rem := x % y
+  fmt.Println(rem) 
+}
