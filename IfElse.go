86b9651dcadabbcf5ddcb0a516e1c496b6bd30de
Emdadul Islam <58410798+mdadul@users.noreply.github.com>
2021-05-11 16:20:30 +0600

    If else condition

---
+++ IfElse.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    
+    var x int = 500
+    if x==500{
+        fmt.Println("Equal")
+    }
+    if x>200{
+        fmt.Println("x is greater than 200")
+    }
+    
+    if x<600{
+        fmt.Println ("x is less than 600")
+    }
+}
